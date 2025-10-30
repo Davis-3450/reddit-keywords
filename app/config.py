cd3972eec3f290555236db1791c54c5b3e4fa366
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-10-30 12:41:24 -0500

    refactor: remove redundant None and update RemovedValues

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import Enum
 from os import getenv
 
@@ -8,6 +9,7 @@ from app.models import UiMode
 load_dotenv()
 
 
+@dataclass
 class Config:
     mode: UiMode = UiMode.TYPER
     debug: bool = False
@@ -15,15 +17,13 @@ class Config:
 
 
 class RedditAccount:
-    username: str | None = getenv("REDDIT_USERNAME") or None
-    client_id: str | None = getenv("REDDIT_CLIENT_ID") or None
-    client_secret: str | None = getenv("REDDIT_CLIENT_SECRET") or None
-    password: str | None = getenv("REDDIT_PASSWORD") or None
+    username: str | None = getenv("REDDIT_USERNAME")
+    client_id: str | None = getenv("REDDIT_CLIENT_ID")
+    client_secret: str | None = getenv("REDDIT_CLIENT_SECRET")
+    password: str | None = getenv("REDDIT_PASSWORD")
     user_agent: str | None = getenv("REDDIT_USER_AGENT") or "reddit-keywords-bot/0.1"
 
 
-class Blacklist(str, Enum):
-    """Tipos de blacklist."""
-
-    SUBREDDITS = "[removed]"
-    USERS = "[deleted]"
+class RemovedValues(str, Enum):
+    REMOVED = "[removed]"
+    DELETED = "[deleted]"
