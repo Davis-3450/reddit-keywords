cfdb54af19104c9613f842c94bfc8b407014af56
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-10-30 05:01:13 -0500

    refactor: improve scraper functionality by enhancing comment handling and increasing search limit

--- app/client/scraper.py
+++ app/client/scraper.py
@@ -3,6 +3,7 @@ from praw.models import Comment as RedditComment
 from praw.models import Subreddit
 from praw.models.reddit.more import MoreComments
 
+from app.config import Blacklist
 from app.models import Comment as CommentModel
 from app.models import Post as PostModel
 from app.utils.exceptions import SubredditNotFound
@@ -17,11 +18,11 @@ class Scraper:
         self,
         subreddit: str | Subreddit,
         keywords: list[str],
-        limit: int = 100,
+        limit: int = 1000,
         include_comments: bool = False,
     ):
         if isinstance(subreddit, str):
-            if not self._validate_existence(subreddit):
+            if not self.validate_existence(subreddit):
                 raise SubredditNotFound()
             sub: Subreddit = self.client.subreddit(subreddit)
         else:
@@ -32,32 +33,42 @@ class Scraper:
         )
 
         for post in sub.search(query, limit=limit):
+            post_info = PostModel(title=post.title, url=post.url, body=post.selftext)
             if include_comments:
                 comments: list[CommentModel] = []
                 for comment in post.comments.list():
                     comments.append(self._build_comments(comment))
+                post_info.comments = comments
 
-            yield PostModel(title=post.title, url=post.url, body=post.selftext)
+            yield post_info
 
-    def _build_comments(self, reddit_comment: RedditComment) -> CommentModel:
-        comment: CommentModel = CommentModel(body=reddit_comment.body)
+    def _build_comments(
+        self, reddit_comment: RedditComment | MoreComments
+    ) -> CommentModel | None:
+        if isinstance(reddit_comment, MoreComments):
+            return None
+
+        if not hasattr(reddit_comment, "body"):
+            return None
+        body = " ".join(reddit_comment.body.split())
+
+        comment: CommentModel = CommentModel(body=body)
 
         if reddit_comment.replies:
             reddit_comment.replies.replace_more(limit=None)
             comment.comments = []
 
             for reply in reddit_comment.replies.list():
-                if (
-                    isinstance(reply, MoreComments)
-                    or not hasattr(reply, "body")
-                    or not reply.body
-                ):
-                    continue
-                comment.comments.append(self._build_comments(reply))
+                built = self._build_comments(reply)
+                if built and built not in Blacklist:
+                    comment.comments.append(built)
+
+        if not comment.comments:
+            comment.comments = None
 
         return comment
 
-    def _validate_existence(self, subreddit: str) -> bool:
+    def validate_existence(self, subreddit: str) -> bool:
         try:
             self.client.subreddit(subreddit).id
             return True
