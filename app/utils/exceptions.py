f52f17db5208e638f1e3710fdd9340d05287258c
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-10-30 04:56:10 -0500

    refactor: enhance exception handling with dataclass for better clarity

--- app/utils/exceptions.py
+++ app/utils/exceptions.py
@@ -1,8 +1,16 @@
-class BaseException(Exception):
+from dataclasses import dataclass
+
+
+@dataclass
+class AppException(Exception):
+    message: str
     pass
 
+    def __str__(self) -> str:
+        return self.message
 
-class SourcesNotFound(BaseException):
+
+class SourcesNotFound(AppException):
     def __init__(
         self,
         message: str = "No valid source files found. Please provide at least one valid .txt file path.",
@@ -11,10 +19,10 @@ class SourcesNotFound(BaseException):
         super().__init__(self.message)
 
 
-class SubredditNotFound(BaseException):
+class SubredditNotFound(AppException):
     def __init__(
         self,
         message: str = "The specified subreddit was not found. Please check the subreddit name and try again.",
-    ):
+    ) -> None:
         self.message = message
         super().__init__(self.message)
