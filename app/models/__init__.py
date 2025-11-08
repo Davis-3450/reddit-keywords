49b17e5fd692361557c53d6798dc5aa6a407e6d7
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-11-08 02:56:54 -0500

    feat: add __len__ method to for post len calculation

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -12,6 +12,11 @@ class Comment:
     body: str
     comments: list["Comment"] | None = None
 
+    def __len__(self) -> int:
+        return len(self.body) + (
+            sum(len(c) for c in self.comments) if self.comments else 0
+        )
+
 
 @dataclass
 class Post:
@@ -19,3 +24,10 @@ class Post:
     url: str | None = None
     body: str | None = None
     comments: list[Comment] | None = None
+
+    def __len__(self) -> int:
+        return (
+            len(self.title)
+            + (len(self.body) if self.body else 0)
+            + (sum(len(c) for c in self.comments) if self.comments else 0)
+        )
