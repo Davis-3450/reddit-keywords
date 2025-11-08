d53850692ac2c442292e4d05f140ca145c72361e
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-11-08 02:57:41 -0500

    refactor: enhance scraper comment processing and improve logging for scraped posts

--- app/client/scraper.py
+++ app/client/scraper.py
@@ -1,12 +1,15 @@
+from typing import Generator
+
 from praw import Reddit
 from praw.models import Comment as RedditComment
 from praw.models import Subreddit
 from praw.models.reddit.more import MoreComments
 
-from app.config import Blacklist
+from app.config import RemovedValues
 from app.models import Comment as CommentModel
 from app.models import Post as PostModel
 from app.utils.exceptions import SubredditNotFound
+from app.utils.print import p
 
 
 class Scraper:
@@ -20,7 +23,7 @@ class Scraper:
         keywords: list[str],
         limit: int = 1000,
         include_comments: bool = False,
-    ):
+    ) -> Generator[PostModel, None, None]:
         if isinstance(subreddit, str):
             if not self.validate_existence(subreddit):
                 raise SubredditNotFound()
@@ -37,13 +40,17 @@ class Scraper:
             if include_comments:
                 comments: list[CommentModel] = []
                 for comment in post.comments.list():
-                    comments.append(self._build_comments(comment))
+                    built = self._build_comments(comment)
+                    if built:
+                        comments.append(built)
                 post_info.comments = comments
-
+            p.info(f"Scraped post: {post_info.title} from r/{sub.display_name}")
             yield post_info
 
     def _build_comments(
-        self, reddit_comment: RedditComment | MoreComments
+        self,
+        reddit_comment: RedditComment | MoreComments,
+        # whitelist: set
     ) -> CommentModel | None:
         if isinstance(reddit_comment, MoreComments):
             return None
@@ -53,14 +60,16 @@ class Scraper:
         body = " ".join(reddit_comment.body.split())
 
         comment: CommentModel = CommentModel(body=body)
-
         if reddit_comment.replies:
             reddit_comment.replies.replace_more(limit=None)
             comment.comments = []
 
             for reply in reddit_comment.replies.list():
                 built = self._build_comments(reply)
-                if built and built not in Blacklist:
+                if (built and built.body) and not (
+                    (RemovedValues.REMOVED in built.body)
+                    or (RemovedValues.DELETED in built.body)
+                ):
                     comment.comments.append(built)
 
         if not comment.comments:
